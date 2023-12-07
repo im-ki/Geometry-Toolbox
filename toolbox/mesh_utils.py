bd8cb2cf98c6c22b8e30b39a6613e5fcc1119b72
im-ki <im.ki@foxmail.com>
2023-12-07 12:53:18 +0800

    add compute_curvature function to mesh_utils.py

--- toolbox/mesh_utils.py
+++ toolbox/mesh_utils.py
@@ -125,16 +125,119 @@ def perform_mesh_smoothing(face, vertex, f, type = 'combinatorial'):
 
     return f
 
-if __name__ == '__main__':
-    from scipy.io import loadmat
-    data = loadmat('/mnt/c/Users/qgchen/Documents/workspace/PhD/3D_Beltrami/3DQCLR/test_3d_tet/step1/outer_mesh.mat')
-    face = data['face'] - 1
-    vert = data['vert']
-    print(face.shape, vert.shape)
-    
-    normalv, normalf = compute_normal(vert, face)
+def compute_curvature(vert, face):
+    """
+    Umin is the direction of minimum curvature
+    Umax is the direction of maximum curvature
+    Cmin is the minimum curvature
+    Cmax is the maximum curvature
+    Cmean=(Cmin+Cmax)/2
+    Cgauss=Cmin*Cmax
+    Normal is the normal to the surface
 
-    f = np.arange(vert.shape[0])
-    new_f = perform_mesh_smoothing(face, vert, f)
-    print(f[:10])
-    print(new_f[:10, :])
+    This is a reimplementation of the curvature computation algorithm described in
+        David Cohen-Steiner and Jean-Marie Morvan.
+        Restricted Delaunay triangulations and normal cycle.
+        In Proc. 19th Annual ACM Symposium on Computational Geometry,
+        pages 237-246, 2003.
+    and also in
+        Pierre Alliez, David Cohen-Steiner, Olivier Devillers, Bruno Le�vy, and Mathieu Desbrun.
+        Anisotropic Polygonal Remeshing.
+        ACM Transactions on Graphics, 2003.
+        Note: SIGGRAPH '2003 Conference Proceedings
+    """
+    assert(vert.shape[1] == 3 and face.shape[1] == 3)
+    orient = 1
+    Nf, Nv = face.shape[0], vert.shape[0]
+    face = face.astype(np.int64)
+
+    bit = len(str(Nv))
+    box = 10**bit
+
+    I = np.concatenate((face[:, 0], face[:, 1], face[:, 2]))
+    J = np.concatenate((face[:, 1], face[:, 2], face[:, 0]))
+    S = np.concatenate((np.arange(1, Nf+1), np.arange(1, Nf+1), np.arange(1, Nf+1)))
+
+    # associate each edge to a pair of faces
+    mask = (I < J)
+    mask_inv = np.logical_not(mask)
+    I1, J1, S1 = I[mask], J[mask], S[mask]
+    I2, J2, S2 = J[mask_inv], I[mask_inv], S[mask_inv] # Here we reverse the order of I and J to ensure that I2 < J2.
+
+    set1, set2 = I1*box+J1, I2*box+J2
+    IJ, x_ind, y_ind = np.intersect1d(set1, set2, return_indices=True)
+    S1, S2 = S1[x_ind], S2[y_ind]
+
+    J = IJ % box
+    I = (IJ - J) // box
+    S = np.vstack((S1, S2)).T - 1
+    Ne = I.shape[0]
+
+    # Normalize edge
+    edge = vert[J, :] - vert[I, :]
+    d = np.linalg.norm(edge, axis = 1)
+    edge = edge / d.reshape((-1, 1))
+    # avoid too large numerics
+    d = d / np.mean(d)
+
+    # normals to vertices and faces
+    normalv, normal = compute_normal(vert, face)
+    # inner product of normals
+    dp = np.sum(normal[S[:, 0], :] * normal[S[:, 1], :], axis = 1)
+    # angle un-signed
+    beta = np.arccos(np.clip(dp, -1, 1))
+    # compute the sign
+    cp = np.cross(normal[S[:, 0], :], normal[S[:, 1], :])
+    si = orient * np.sign(np.sum(cp * edge, axis = 1))
+    # angle signed
+    beta = beta * si
+
+    # tensors
+    T = np.zeros((Ne, 3, 3))
+    # outer product, since it is a simmetric rank-1 matrix, we only need to compute part of them
+    for x in range(3):
+        for y in range(x+1):
+            T[:, x, y] = edge[:, x] * edge[:, y]
+            T[:, y, x] = T[:, x, y]
+    T = T * np.reshape(d*beta, (-1, 1, 1)) # apply weights
+
+    # do pooling on vertices, w record the number of edges incident to the vertices
+    Tv = np.zeros((Nv, 3, 3))
+    w = np.zeros(Nv)
+    for i in range(Ne):
+        Tv[I[i], :, :] = Tv[I[i], :, :] + T[i, :, :]
+        Tv[J[i], :, :] = Tv[J[i], :, :] + T[i, :, :]
+        w[I[i]] = w[I[i]] + 1
+        w[J[i]] = w[J[i]] + 1
+    w[w < np.finfo(float).eps] = 1
+    Tv = Tv / w.reshape((-1, 1, 1))
+
+    # do averaging to smooth the field
+    for x in range(3):
+        for y in range(3):
+            Tv[:, x, y] = perform_mesh_smoothing(face, vert, Tv[:, x, y]).reshape(-1)
+
+    # extract eigenvectors and eigenvalues
+    U = np.zeros((Nv, 3, 3))
+    D = np.zeros((Nv, 3))
+    val, vec = np.linalg.eig(Tv)
+    ind = np.argsort(np.abs(val), axis = 1)
+    D = np.take_along_axis(val, ind, axis = 1)
+    ind = np.expand_dims(ind, axis = 1)
+    U = np.take_along_axis(vec, ind, axis = 2)
+
+    Normal = U[:, :, 0]
+    # According to the paper "Anisotropic Polygonal Remeshing", the associated directions of Cmin and Cmax are switched.
+    Umin = U[:, :, 2]
+    Umax = U[:, :, 1]
+    Cmin = D[:, 1]
+    Cmax = D[:, 2]
+    Cmean = (Cmin+Cmax) / 2
+    Cgauss = (Cmin * Cmax)
+
+    # Try to re-orient the normals
+    s = np.sign(np.sum(Normal * normalv, axis = 1))
+    Normal = Normal * s.reshape((-1, 1))
+    return Umin, Umax, Cmin, Cmax, Cmean, Cgauss, Normal
+
+
