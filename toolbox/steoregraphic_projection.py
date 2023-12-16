19308bbce33a39a95ab672190f161274bd23a496
im-ki <im.ki@foxmail.com>
2023-12-16 10:28:52 +0800

    add north pole steoregraphic proj

--- toolbox/steoregraphic_projection.py
+++ toolbox/steoregraphic_projection.py
@@ -1,9 +1,20 @@
 import numpy as np
 
-def steoregraphic_proj_inv(org_v):
+
+def steoregraphic_proj(v, keep_direction = False):
+    # keep_direction: North pole steoregraphic projection will change the direction of the triangles. To correct the direction, set this flag.
+    x = v[:, 0] / (1-v[:, 2])
+    y = v[:, 1] / (1-v[:, 2])
+    if keep_direction:
+        y = -y # To preserve the vertices direction of the faces
+    return np.stack((x, y)).T
+
+def steoregraphic_proj_inv(org_v, keep_direction = False):
+    # keep_direction: North pole steoregraphic projection will change the direction of the triangles. To correct the direction, set this flag.
     assert org_v.shape[1] == 2
     v = org_v.copy()
-    #v[:, 1] = -v[:, 1]
+    if keep_direction:
+        v[:, 1] = -v[:, 1]
     x = (2*v[:,0]) / (1 + v[:,0]**2 + v[:,1]**2)
     y = (2*v[:,1]) / (1 + v[:,0]**2 + v[:,1]**2)
     z = (-1 + v[:,0]**2 + v[:,1]**2) / (1 + v[:,0]**2 + v[:,1]**2)
